2973ffc4f72b385f555716b29d873f4eae8e3353
yogita-s-24 <yogitashete24@gmail.com>
2023-11-22 01:05:26 +0530

    simple lines of text added

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import './Home.css'
 
 function Home() {
+  const userCount = 15;
   return (<>
- <h1>Home Page</h1>
+  <h1>WelCome!</h1>
+  <p>We you enjoy learning React.</p>
+  <h3>Have a greate day.</h3>
+  <h4>{userCount} users are learning this session.</h4>
   </>
     )
 }
