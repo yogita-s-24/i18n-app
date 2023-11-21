3345136e04a8b01ede3a0265d9bbc2885a9b7e70
yogita-s-24 <yogitashete24@gmail.com>
2023-11-22 00:39:57 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './views/Home/Home.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <Home />
+    
+  </>
+)
---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (<>
+ <h1>Home Page</h1>
+  </>
+    )
+}
+
+export default Home
