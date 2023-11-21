ed4654fa3f671a3c92a6093069dec9c7cec2b2f8
yogita-s-24 <yogitashete24@gmail.com>
2023-11-22 01:42:44 +0530

    refactor: Implement language-specific content in Home component

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './Home.css'
+import I18n from '../../utils/i18n';
 
 function Home() {
   const userCount = 15;
   return (<>
-  <h1>WelCome!</h1>
-  <p>We you enjoy learning React.</p>
-  <h3>Have a greate day.</h3>
-  <h4>{userCount} users are learning this session.</h4>
+  <h1>{I18n("welcomeMessage")}</h1>
+  <p>{I18n("normalMessage")}</p>
+  <h3>{I18n("greetingMessage")}</h3>
+
+  
+  {/* <h4>{userCount} users are learning this session.</h4> */}
   </>
     )
 }
