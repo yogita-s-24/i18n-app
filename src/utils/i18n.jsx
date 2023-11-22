ed266b56bfe207041d9bd4541e2476df1e5f0df0
yogita-s-24 <yogitashete24@gmail.com>
2023-11-22 13:50:35 +0530

    feat: Add I18n function for internationalization support

--- src/utils/i18n.jsx
+++ src/utils/i18n.jsx
@@ -1,7 +1,14 @@
 import languageConfig from './lang.json'
 
-const I18n = (keyword, lang) =>{
-    return languageConfig[lang] [keyword];
+const I18n = (keyword, placeholder, value) =>{
+    const lang = localStorage.getItem("lang") || "en";
+    const text = languageConfig[lang][keyword];
+
+    if(placeholder && value){
+        return text.replace(placeholder, value);
+    }
+
+    return text;
 }
 
 export default I18n;
