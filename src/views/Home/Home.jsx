2a6d464dee18dafe3defb07db870c52a6e829ef5
yogita-s-24 <yogitashete24@gmail.com>
2023-11-22 13:53:00 +0530

    feat: Implement language selection dropdown for dynamic content translation and store value in local storage

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
-import I18n from '../../utils/i18n';
+import I18n from './../../utils/i18n.jsx';
 
 function Home() {
+  const [lang, setLang] = useState("mr");
   const userCount = 15;
   return (<>
   <h1>{I18n("welcomeMessage")}</h1>
   <p>{I18n("normalMessage")}</p>
-  <h3>{I18n("greetingMessage")}</h3>
+  <h3>{I18n( "greetingMessage")}</h3>
+  <h5>{I18n("endMessage")}</h5>
 
-  
-  {/* <h4>{userCount} users are learning this session.</h4> */}
+  <select
+   defaultValue={localStorage.getItem('lang')} 
+   onChange={(e)=>{
+    localStorage.setItem('lang',e.target.value);
+    window.location.reload();
+  }}>
+    <option value="mr">Marathi</option>
+    <option value="en">English</option>
+    <option value="hi">Hindi</option>
+  </select>
+
+
+  <h4>{I18n("userStatusMessage","<studentCount>", userCount)}</h4>
   </>
-    )
+  )
 }
 
 export default Home
